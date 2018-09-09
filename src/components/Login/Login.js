723685ae8d6fc91a13745b4e700fa0389937ab40
Cas <cas@interglobal.vision>
2018-09-09 14:46:20 -0500

    add singup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,47 +5,97 @@ import { Link } from 'react-router-dom';
 
 export class LoginForm extends Component {
 	state = {
+    signup: false,
 		email: '',
+    username: '',
 		password: '',
+		passwordConfirm: '',
 		error: {
 			message: '',
 		},
 		isLoading: false,
 	}
 
+  constructor(props) {
+    super(props);
+
+    // BINDING
+    this.toggleSignup = this.toggleSignup.bind(this);
+  }
+
 	componentDidMount() {
 		this.email.focus()
 	}
 
 	login() {
-		const { email, password } = this.state;
+		const { email, username, password, passwordConfirm } = this.state;
 
 		this.setState({ isLoading: true });
 
-		this.props.firebase
-			.login({email, password})
-			.then(() => {
-				this.setState({ isLoading: false })
-				// this is where we can redirect to another route
-			})
-			.catch((error) => {
-				this.setState({ isLoading: false })
-				this.setState({ error });
-				console.log('there was an error', error)
-			})
+    // SINGUP
+    if (this.state.signup) {
+      this.props.firebase
+        .createUser({
+          email,
+          password,
+        }, {
+          username,
+          email,
+        })
+        .then(() => {
+          this.setState({ isLoading: false })
+          // this is where we can redirect to another route
+        })
+        .catch((error) => {
+          this.setState({ isLoading: false })
+          this.setState({ error });
+          console.log('there was an error', error)
+        });
+
+    } else { // LOGIN
+
+      this.props.firebase
+        .login({email, password})
+        .then(() => {
+          this.setState({ isLoading: false })
+          // this is where we can redirect to another route
+        })
+        .catch((error) => {
+          this.setState({ isLoading: false })
+          this.setState({ error });
+          console.log('there was an error', error)
+        });
+
+    }
 
 	}
+
+  toggleSignup() {
+    this.setState( state => ({
+      signup: !state.signup,
+    }));
+  }
 
 	render() {
 		return (
 			<section className='container'>
 				<div className='grid-row margin-top-basic margin-bottom-small'>
 					<div className='grid-item item-s-24'>
-						<h1>Iniciar Sesión</h1>
+						<h1>{ this.state.signup ? 'Signup' : 'Login' }</h1>
 					</div>
 				</div>
         <form onSubmit={event => event.preventDefault()} className='grid-row margin-bottom-basic'>
           <div className='grid-item item-s-24 margin-bottom-micro no-gutter'>
+          { this.state.signup ?
+            <div className='grid-item item-s-24 margin-bottom-tiny no-gutter'>
+              <input
+                ref={ ref => this.username = ref}
+                type='text'
+                placeholder='username'
+                onChange={event => this.setState({ username: event.target.value })}
+              />
+            </div>
+          : null }
             <input
               ref={ ref => this.email = ref}
               type='text'
@@ -59,17 +109,32 @@ export class LoginForm extends Component {
               ref={ ref => this.password = ref}
               type='password'
               placeholder='password'
-              autoComplete='current-password'
+              autoComplete={ this.state.signup ? '' : 'current-password'}
               onChange={event => this.setState({ password: event.target.value })}
             />
-            <Link id="link-forgot-password" className='font-size-small color-grey-mid link-underline' to={'/forgotpassword'}>Forgot?</Link>
+          </div>
+          { this.state.signup ?
+            <div className='grid-item item-s-24 margin-bottom-tiny no-gutter'>
+              <input
+                ref={ ref => this.passwordConfirm = ref}
+                type='password'
+                placeholder='confirm password'
+                onChange={event => this.setState({ passwordConfirm: event.target.value })}
+              />
+            </div>
+          : null }
+          <div className='grid-item item-s-12 margin-bottom-tiny no-gutter'>
+            <button className='font-size-small color-grey-mid link-underline' onClick={() => this.toggleSignup()}>{this.state.signup ? 'Login' : 'Signup'}</button>
+          </div>
+          <div className='grid-item item-s-12 margin-bottom-tiny text-align-right'>
+            <Link id="link-forgot-password" className='font-size-small color-grey-mid link-underline' to={'/forgotpassword'}>Forgot password?</Link>
           </div>
           <div className='grid-item item-s-8 offset-s-16 text-align-center no-gutter'>
-            <button onClick={() => this.login()} className='font-size-large button-link-style'>
+            <button onClick={() => this.login()} className='font-size-large'>
               Login
             </button>
           </div>
-          <div className='grid-item item-s-24 no-gutter'>
+          <div className='grid-item item-s-24'>
             <span>{this.state.error.message}</span>
             <span>{this.state.loading}</span>
           </div>

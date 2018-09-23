cd5a87d0b8fc82343b0a172251fd81a92b8caa3a
Cas <cas@interglobal.vision>
2018-09-23 12:45:43 -0500

    add gray color to subs header

--- src/components/Subscriptions/SubscriptionsHeader.js
+++ src/components/Subscriptions/SubscriptionsHeader.js
@@ -4,19 +4,18 @@ import injectSheet from 'react-jss'
 import { Route, Switch, withRouter } from 'react-router-dom';
 
 // STYLES
-import { colorBlue, colorWhite } from 'styl/constants';
+import { colorGray, colorWhite } from 'styl/constants';
 
 const styles = {
- backgroundBlue: {
-   backgroundColor: colorBlue,
-   color: colorWhite,
+ backgroundGray: {
+   backgroundColor: colorGray,
  },
 };
 
 const SubscriptionsSectionHeader = ({ classes }) => (
-  <div className={`grid-row padding-top-micro padding-bottom-micro`}>
+  <div className={`grid-row padding-top-micro padding-bottom-micro ${classes.backgroundGray}`}>
     <Link className='grid-item' to={'subscriptions/add'}>New</Link>
   </div>
 );
 
-export default SubscriptionsSectionHeader;
+export default injectSheet(styles)(SubscriptionsSectionHeader);
--- src/styl/constants.js
+++ src/styl/constants.js
@@ -3,3 +3,4 @@ export const colorBlue = 'rgba(120, 190, 255)';
 export const colorOrange = 'rgb(251, 103, 54)';
 export const colorYellow = 'rgb(255, 193, 31)';
 export const colorWhite = 'rgb(255, 255, 255)';
+export const colorGray = 'rgb(244, 245, 246)';

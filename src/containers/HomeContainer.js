a095af5e9e2b017d4291d18b81211dede3fb81cc
Cas <cas@interglobal.vision>
2018-12-03 16:22:57 -0600

    change in home container to use rank

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { firebaseConnect } from 'react-redux-firebase';
+import { isLoaded, firebaseConnect } from 'react-redux-firebase';
 
 // import SubscriptionsHeader from 'components/Subscriptions/SubscriptionsHeader';
 import Playlist from 'components/Playlist/Playlist.js';
 
-const HomeContainer = ({ tracks }) => (
-  <section id="tracks">
-    <Playlist tracks={tracks} />
-  </section>
-);
+const HomeContainer = ({ chart }) => {
+  // Message for if todos are loading
+  if(!isLoaded(chart)) {
+    return <span>Loading...</span>
+  }
 
-const maptStateToProps = ({ firebase: { ordered } }) => ({
-  tracks: ordered.tracks,
-});
+  return (
+    <section id="tracks">
+      <Playlist tracks={chart.latest.tracks} />
+    </section>
+  )
+}
+
+const maptStateToProps = ({ firebase }) => {
+  return ({
+    chart: firebase.ordered.chart
+  });
+}
 
 export default compose(
   firebaseConnect([{
-    path: 'tracks',
-    queryParams: ['orderByChild=status', `equalTo=published`],
+    path: 'chart/latest/tracks',
   }]),
   connect(
     maptStateToProps,
